94a954d01d1cbfd8291ab9545feeb29e17a28998
vagrant <vagrant@homestead>
2022-12-07 22:47:28 +0000

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<x-header />
+<div class="w-full max-w-lg m-auto">
+    <div class="flex flex-row gap-3 m-2">
+            <a href="/folder"><x-monoicon-folder-add class="rounded w-10 h-10 my-auto hover:text-white hover:bg-blue-600" /></a>
+            <form action="" method="GET" class=" w-full">
+                <input class="border-2 rounded-md w-full p-2 font-mono text-center" placeholder="search" type="text" name=""
+                    id="">
+            </form>
+    </div>
+
+</div>
+<x-footer />
