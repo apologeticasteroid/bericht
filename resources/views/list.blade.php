a5a78875ced48f3f189fc90ac7dde2c98fe8034d
vagrant <vagrant@homestead>
2022-12-13 15:49:06 +0000

    Removed file and folder pages, reverting project to work as distributor only, initially

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -14,33 +14,14 @@
 {{-- Section: List of folders and reports possibly filtered by search --}}
 <div class="w-full font-mono max-w-2xl m-auto my-5">
     <div class="flex flex-row p-2">
-        <a class="hover:bg-gray-400 rounded-md p-2" href="/file/x">
+        <div class="p-2" >
             <span class="text-2xl font-bold ">Central Bank</span>
-        </a>
-        <a class="p-2 rounded-md hover:text-white hover:bg-green-600" href="/file">
-            <x-monoicon-document-add class="rounded w-8 h-8" />
-        </a>
+        </div>
     </div>
 
     <ul class="m-2">
         <li class="flex flex-row gap-3">
             <a class="hover:bg-gray-400 rounded-md p-2" href="/file/1">
-                <div>CUR12_A-Related party balances.</div>
-            </a>
-            <a class="p-2 hover:text-white hover:bg-blue-600 rounded-md" href="/download/x">
-                <x-monoicon-download class="rounded w-5 h-5" />
-            </a>
-        </li>
-        <li class="flex flex-row gap-3">
-            <a class="hover:bg-gray-400 rounded-md p-2" href="/file/2">
-                <div>CUR12_A-Related party balances.</div>
-            </a>
-            <a class="p-2 hover:text-white hover:bg-blue-600 rounded-md" href="/download/x">
-                <x-monoicon-download class="rounded w-5 h-5" />
-            </a>
-        </li>
-        <li class="flex flex-row gap-3">
-            <a class="hover:bg-gray-400 rounded-md p-2" href="/file/3">
                 <div>CUR12_A-Related party balances.</div>
             </a>
             <a class="p-2 hover:text-white hover:bg-blue-600 rounded-md" href="/download/x">
